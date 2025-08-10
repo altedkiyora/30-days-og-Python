3196088635bd54cdfd3bfa0eaff1bea37b87ba7c
altedkiyora <isntshourov@gmail.com>
2025-08-10 20:27:37 +0600

    done d1

--- D1.py
+++ D1.py
@@ -4,4 +4,17 @@
 "okay"
 """kaayayy
 afkafauuuuafjfklf"""
-type(10)
+print(type(10)) # int
+type(4-4j) # wont print cause of no cmd
+print(type({'name':'sh'}))
+print(type(
+    {'test':
+    [
+        'first','time','here'
+    ]
+}
+))
+print({
+'euclidian distance':((2-10)**2)+((3-8)**2)
+})
+# d1 done
