9e2a86f467b60ef689f21e3813f5ef874915ae4b
altedkiyora <isntshourov@gmail.com>
2025-08-11 18:16:06 +0600

    day 2 doneeee

---
+++ D2.py
@@ -0,0 +1,40 @@
+#Start
+print('hello, world!')
+print(len('hello, world!'))
+print(
+    type('hello, world!')
+)
+first_try = input(
+            'Enter your name:')
+print(
+    first_try
+)
+print('is this first?', first_try)
+
+str_ = '19'
+int__ = int(str_)
+print(int__)
+
+__str = '1'
+__int = int(__str)
+print('integar:',__int)
+
+first_name = 'sho'
+first_name_to_list = list(first_name)
+print(first_name_to_list)
+print(first_name)
+
+first, second = 1, 2
+print(first,second)
+print(type(first))
+print(len(first_name))
+first_input = input('input?')
+print(first_input)
+
+r = input('radius?')
+true_r = float(r)
+pi = 3.1417
+area = pi*true_r**2.0000
+print('Area is',area)
+help('keywords')
+#d2 done
