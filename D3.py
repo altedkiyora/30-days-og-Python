b5ee49352c95c78ba595edcca151c52f390b3858
altedkiyora <isntshourov@gmail.com>
2025-08-12 18:15:30 +0600

    is this ggoooodd?

---
+++ D3.py
@@ -0,0 +1,45 @@
+print(True)
+print(1 is 2)
+print( 's' in 'Ahsanullah')
+integar_age = 200
+float_height = 200.2
+complex_mun= 6+1j
+
+b = input( 'Enter base')
+base = float(b)
+print(b)
+h = input( 'Enter height')
+height=float(h)
+print(h)
+c = 0.5
+cons = float(c)
+area = base * height * cons
+print('The area og triangle is',area)
+
+print(len('python') != len('dragon'))
+
+#Need to start the vids
+
+
+print('on' in ('python' and 'dragon'))
+print(not('on' in ('python' and 'dragon')))
+valuex = input('enter number')
+x = int(valuex)
+remainder_check = x % 2
+print((x / 2) and (remainder_check == 0))
+int
+
+
+x1, x2, y1, y2= 2,6,2,10
+float(x1)
+float(x2)
+float(y1)
+float(y2)
+m = (y2 - y1) / (x2 - x1)
+print(m)
+
+valueX=input("enter x")
+X = int(valueX)
+y= (X ** 2) + (6 * X) + 9
+print(y == 0)
+
