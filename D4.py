b59ff286c61ae35f53cefe348e7f7b6a7854fada
altedkiyora <isntshourov@gmail.com>
2025-08-13 18:12:04 +0600

    not done today

---
+++ D4.py
@@ -0,0 +1,27 @@
+#strings
+gull = "okkkk"
+full = "nootokakakakaka" 
+print(gull + full)
+print ("\\")
+print ("Day1\tPuthon")
+print('\" Double Quote \"')
+print ("Day1\t\tputhon")
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formated_string)
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+
+language = 'puthon'
+print (language[-6])
+language = 'Python'
+pto = language[0:6:2] #
+print(pto) # Pto
