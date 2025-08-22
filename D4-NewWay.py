1fcbfc456d85d0659a3e20abfcc01eda8c954119
altedkiyora <isntshourov@gmail.com>
2025-08-22 11:23:08 +0600

    lesss start

--- D4-NewWay.py
+++ D4-NewWay.py
@@ -6,3 +6,7 @@ sum = a + b
 txt = "text"
 print(2*sum*4)
 print(2*txt*4)
+# Python doesn't execute comments
+""" age = int(input("whatever:")) will take input and assign it to name and the dtype will be int. """
+age = int(input("whatever:"))
+print("My age: ", age)
