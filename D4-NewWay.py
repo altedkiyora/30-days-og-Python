696b2187c75a147799efd8419e71186614b97872
altedkiyora <isntshourov@gmail.com>
2025-08-24 18:16:01 +0600

    taking time

--- D4-NewWay.py
+++ D4-NewWay.py
@@ -1,14 +1,29 @@
-a = None
-#hehehe
-a = 3
-b = 6
-sum = a + b
-txt = "text"
-print(2*sum*4)
-print(2*txt*4)
-# Python doesn't execute comments
-""" age = int(input("whatever:")) will take input and assign it to name and the dtype will be int. """
-age = int(input("whatever:"))
-print("My age: ", age)
+# a = None
+# #hehehe
+# a = 3
+# b = 6
+# sum = a + b
+# txt = "text"
+# print(2*sum*4)
+# print(2*txt*4)
+# # Python doesn't execute comments
+# """ age = int(input("whatever:")) will take input and assign it to name and the dtype will be int. """
+# age = int(input("whatever:"))
+# print("My age: ", age)
 
-# operators today
+# # operators today
+# value1 = int(input("1st number"))
+# value2 = int(input("2nd number"))
+# print("first value",value1)
+# print("second value",value2)
+# sum = value1 + value2
+# print("sum will be ", sum)
+
+# a = float(input("The side: "))
+# print("area will be ", a ** 2)
+
+a = float(input("first"))
+b = float(input("second"))
+avg = (a + b)/2
+print((a == b) or (a >b))
+          
