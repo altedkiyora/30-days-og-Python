1e703f9b603806ddac0647c5a66dc94ebc6f9cff
altedkiyora <isntshourov@gmail.com>
2025-09-05 20:24:23 +0600

    done

--- Newday.py
+++ Newday.py
@@ -1,2 +1,14 @@
 #TRY
 #This is it.
+#Length space as well 
+# namin = input("ENTER name: ")
+# print (len(namin))
+# str = "Iam $"
+# print(str.count(str))
+# marks = int(input("Enter: "))
+# if(marks >= 90):
+#     print("Grade A")
+# elif(marks >= 80 and marks < 90 ):
+#     print("Grade B")
+# else:
+#     print("Failed")
