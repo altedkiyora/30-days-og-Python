68d9f63cfe6db5470232aedc555b4dd5ef47c521
altedkiyora <isntshourov@gmail.com>
2025-09-12 16:21:44 +0600

    done

--- Newday.py
+++ Newday.py
@@ -56,3 +56,12 @@
 # print(f'{a} % {b} = {a % b}')
 # print(f'{a} // {b} = {a // b}')
 # print(f'{a} ** {b} = {a ** b}') 
+# language = 'Python'
+# a,b,c,d,e,f = language # unpacking sequence characters into variables
+# print(a) # P
+# print(b) # y
+# print(c) # t
+# print(d) # h
+# print(e) # o
+# print(f) # n
+# print(language)
